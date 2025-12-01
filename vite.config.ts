6bab43a7436b0488775d502c9fb3e203d4f94f5a
SouthportAI2 <southportai2@gmail.com>
2025-12-01 01:33:26 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-{
-  "rewrites": [
-    {
-      "source": "/(.*)",
-      "destination": "/index.html"
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    build: {
+      target: 'esnext',
+      outDir: 'dist',
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY || process.env.API_KEY),
     }
-  ],
-  "outputDirectory": "dist"
-}
+  };
+});
